ca11052b45590080b188fa681a4a1cb9e01cde52
yurishtro92 <82029839+yurishtro92@users.noreply.github.com>
2022-09-04 13:52:36 +0300

    Add files via upload
    modified

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 import uvicorn
+import json
 
 app = FastAPI()
 
 with open('templates/empty.html', 'r') as f:
     form = f.read()
-
 
 @app.get("/")
 async def root():
@@ -16,10 +16,13 @@ async def root():
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    i = 0
     while True:
         data = await websocket.receive()
-        if data['text'] != 'end':
-            await websocket.send_json(data['text'])
+        data_json = json.dumps(data)
+        if data["text"] != 'end':
+            i += 1
+            await websocket.send_json(str(i) + '. ' + str(json.loads(data_json)['text']))
         else:
             await websocket.send_json('End of work, please press F5 to restart')
             return
